6f4e435a549fef13210af1c3cf2667df23383c20
nellnelaa <nainaijinan@gmail.com>
2024-10-29 01:27:07 +0700

    update baru

--- 24001182-km7-nai-aut-ch5/prisma/migrations/20241028182527_/migration.sql
+++ 24001182-km7-nai-aut-ch5/prisma/migrations/20241028182527_/migration.sql
@@ -83,6 +83,21 @@ CREATE TABLE "specs" (
     CONSTRAINT "specs_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "users" (
+    "id" BIGSERIAL NOT NULL,
+    "name" VARCHAR NOT NULL,
+    "email" VARCHAR NOT NULL,
+    "password" VARCHAR NOT NULL,
+    "profile_picture" VARCHAR,
+    "role_id" INTEGER NOT NULL DEFAULT 2,
+
+    CONSTRAINT "users_pk" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_idx" ON "users"("email");
+
 -- AddForeignKey
 ALTER TABLE "car_details" ADD CONSTRAINT "car_details_cars_id_fkey" FOREIGN KEY ("cars_id") REFERENCES "cars"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
